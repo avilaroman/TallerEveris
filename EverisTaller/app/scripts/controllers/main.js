6a7c8c9d89ce0e9ca04058752e37b1056ff6e7e9
smonardes <sebastian.monardes@gmail.com>
2015-07-07 16:58:38 -0300

    Primera version proyecto

---
+++ EverisTaller/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name everisTallerApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the everisTallerApp
+ */
+angular.module('everisTallerApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
